2967331d2eedaee61242c5c6114fd6b0b6ea8f16
Paul-Jeanroy <pauljeanroy@outlook.fr>
2024-05-24 18:41:02 +0200

    Initial COMMIT LE 24/05

---
+++ src/app/Components/Header/page.jsx
@@ -0,0 +1,18 @@
+/* COMPOSANT HEADER
+
+    FAIT PAR PJ LE 24/05
+
+*/
+
+// Import css
+import "@/app/Css/Header.css"
+
+
+export default function Header(){
+    return (
+        <header>
+            <h1 className="div_titre_site">Projet livret numérique</h1>
+        </header>
+    )
+
+}
---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import "./page.css";
+
+import Header from "@/app/Components/Header/page"
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <section className="section_landing">
+                
+            </section>
+        </>
+    );
+}
