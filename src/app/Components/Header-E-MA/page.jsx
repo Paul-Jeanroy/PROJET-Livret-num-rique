38cbdb64d582e66c8436218ca3ed039a7f93ffc5
Paul jeanroy <pauljeanroy@outlook.fr>
2024-05-29 14:02:11 +0200

    COMMIT 29/05 : [PJ] Création du composant Header.

---
+++ src/app/Components/Header-E-MA/page.jsx
@@ -0,0 +1,45 @@
+/* COMPOSANT HEADER
+
+    FAIT PAR PJ LE 24/05
+
+*/
+
+// Import css
+import "@/app/Css/Header-E-MA/Header.css"
+import Link from "next/link"
+
+
+export default function Header_E_MA() {
+    return (
+        <header>
+            <div className="div_container_header">
+                <h1 className="div_titre_site">Livret d'Apprentissage Numérique</h1>
+                <div className="div_nav_header">
+                    <nav className="nav_header">
+                        <div className="div_nav_acceuil">
+                            <img src="Image/icon-accueil.png"></img>
+                            <Link href={"/Accueil"}>Accueil</Link>
+                        </div>
+                        <div className="div_nav_livret">
+                            <img src="Image/icon-livret.png"></img>
+                            <Link href={"/Livret"}>Livret</Link>
+                        </div>
+                        <div className="div_nav_profil">
+                            <div className="div_contour_avatar">
+                                <img src="Image/icon-avatar.png"></img>
+                            </div>
+                            <Link href={"/Profil"}>Profil</Link>
+                        </div>
+                    </nav>
+                    <button className="btn_notif">
+                        <img src="Image/icon-notif.png"></img>
+                    </button>
+                    <button className="btn_deco">
+                        <Link href={"/Connexion"}>Se déconnecter</Link>
+                    </button>
+                </div>
+            </div>
+        </header>
+    )
+
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import "./page.css";
 
-import Header from "@/app/Components/Header/page"
+import Header_E_MA from "@/app/Components/Header-E-MA/page"
 
 export default function Home() {
     return (
         <>
-            <Header />
+            <Header_E_MA />
             <section className="section_landing">
                 
             </section>
